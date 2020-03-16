53ae61cd563a3a533bd490c24d4569b7d3c51518
Zretep <40038349+Zretep@users.noreply.github.com>
2020-03-16 17:22:31 -0600

    Functional login
    -Added LoginCheck
    -Functional login

---
+++ LoginCheck.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class LoginCheck {
+
+    public String login(String username, String password) throws IOException {
+        ArrayList<String> uNList = new ArrayList<String>();
+        ArrayList<String> pWList = new ArrayList<String>();
+        ArrayList<String> type = new ArrayList<String>();
+
+        try {
+            BufferedReader read = new BufferedReader(new FileReader("Resources/login.txt"));
+            String line = read.readLine();
+            while (line != null){
+                String cLine[] = line.split(" ");
+                int i = 0;
+                while (i <= 3){
+                    if (i == 0) {
+                        uNList.add(cLine[0]);
+                    }
+                    else if (i == 1){
+                        pWList.add(cLine[1]);
+                    }
+                    else if (i == 2){
+                        type.add(cLine[2]);
+                    }
+                    i++;
+                }
+                line = read.readLine();
+            }
+            if (username.length() != 0 && password.length() != 0) {
+                int index = uNList.indexOf(username);
+
+                if (uNList.contains(username) & password == pWList.get(index)){
+                System.out.println("You have sucessfully logged in!");
+                }
+            }
+            else{
+                System.out.println("Please register instead.");
+            }
+
+            read.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Account file not found.");
+        }
+        
+        if (uNList.contains(username) == false || pWList.contains(password) == false) {
+            String userType = "null";
+            return userType;
+        }
+        else {
+            String userType = type.get(uNList.indexOf(username));
+            return userType;
+        }
+    }
+}
