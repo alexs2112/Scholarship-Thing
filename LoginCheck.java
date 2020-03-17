44bda2566ce11d9b510f20a628c73cedd82fac53
alexs2112 <47425839+alexs2112@users.noreply.github.com>
2020-03-17 14:38:18 -0600

    did the thing

--- LoginCheck.java
+++ LoginCheck.java
@@ -14,34 +14,27 @@ public class LoginCheck {
             String line = read.readLine();
             while (line != null){
                 String cLine[] = line.split(" ");
-                int i = 0;
-                while (i <= 3){
-                    if (i == 0) {
-                        uNList.add(cLine[0]);
-                    }
-                    else if (i == 1){
-                        pWList.add(cLine[1]);
-                    }
-                    else if (i == 2){
-                        type.add(cLine[2]);
-                    }
-                    i++;
-                }
+                uNList.add(cLine[0]);
+                pWList.add(cLine[1]);
+                type.add(cLine[2]);
                 line = read.readLine();
             }
-			int index = uNList.indexOf(username);
-            if (username.length() != 0 && password.length() != 0 && uNList.contains(username) && password == pWList.get(index)) {
-                JOptionPane.showMessageDialog(null, "You have sucessfully logged in!");
-            }
-            else{
-                JOptionPane.showMessageDialog(null, "Please register instead.");
-            }
-
             read.close();
 
         } catch (FileNotFoundException e) {
             JOptionPane.showMessageDialog(null, "Account file not found.");
         }
+		
+		if (uNList.contains(username)) {
+			int index = uNList.indexOf(username);
+			if (password.equals(pWList.get(index))) {
+				JOptionPane.showMessageDialog(null, "You have sucessfully logged in!");
+			}
+			else
+				JOptionPane.showMessageDialog(null, "Incorrect username or password.");
+		}
+        else
+			JOptionPane.showMessageDialog(null, "Incorrect username or password.");
         
         if (uNList.contains(username) == false || pWList.contains(password) == false) {
             String userType = "null";
