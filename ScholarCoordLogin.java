e50f16256da199d11a5497e7a5be6fd5a1106eca
brittpysar <43663838+brittpysar@users.noreply.github.com>
2020-03-20 16:29:01 -0600

    Update ScholarCoordLogin.java

--- ScholarCoordLogin.java
+++ ScholarCoordLogin.java
@@ -1,16 +1,17 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextPane;
+import javax.swing.JEditorPane;
+import javax.swing.JTextArea;
+import javax.swing.JToolBar;
 
 public class ScholarCoordLogin extends JFrame {
-
-	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -34,10 +35,27 @@ public class ScholarCoordLogin extends JFrame {
 	public ScholarCoordLogin() {
 		setTitle("Scholarship Coordinator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1340, 745);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		setBounds(100, 100, 300, 200);
+		getContentPane().setLayout(null);
+
+		JButton btnNewButton = new JButton("Create New Award");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				NewAward addAward = new NewAward();
+				addAward.setVisible(true);
+			}
+		});
+		btnNewButton.setBounds(17, 19, 178, 29);
+		getContentPane().add(btnNewButton);
+
+		JButton btnNewButton_1 = new JButton("Edit/Delete Existing Award");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ExistingAward editAward = new ExistingAward();
+				editAward.setVisible(true);
+			}
+		});
+		btnNewButton_1.setBounds(17, 53, 224, 29);
+		getContentPane().add(btnNewButton_1);
 	}
 }
