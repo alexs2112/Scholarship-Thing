60f41608f198b7c981438febe5aaf8f011685e2a
alexs2112 <47425839+alexs2112@users.noreply.github.com>
2020-04-10 20:23:02 -0600

    Functionality for Coordinator complete
    Can now delete awards and award awards (either a single one or all of them

--- ExistingAward.java
+++ ExistingAward.java
@@ -32,13 +32,14 @@ public class ExistingAward extends JFrame {
 		//An arraylist of all the scholarships and their associated data
 		ArrayList<Scholarship> scholarships = data.awards();
 		ArrayList<String> listOfNames = new ArrayList<String>();
+		AwardScholarships function = new AwardScholarships(data);
 		
 		for (Scholarship award : scholarships) {
 			listOfNames.add(award.getScholName());
 		}
 
 		JList list = new JList(listOfNames.toArray());
-		list.setBounds(20, 29, 258, 383);
+		list.setBounds(20, 29, 258, 353);
 		contentPane.add(list);
 
 		JButton btnNewButton = new JButton("Edit");
@@ -49,25 +50,47 @@ public class ExistingAward extends JFrame {
 				editThisAward.setVisible(true);
 			}
 		});
-		btnNewButton.setBounds(20, 424, 93, 29);
+		btnNewButton.setBounds(20, 394, 93, 29);
 		contentPane.add(btnNewButton);
 
-		JButton btnNewButton_1 = new JButton("Save and Close");
+		JButton btnNewButton_1 = new JButton("Exit");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 				data.saveData();
 			}
 		});
-		btnNewButton_1.setBounds(190, 424, 93, 29);
+		btnNewButton_1.setBounds(190, 394, 93, 29);
 		contentPane.add(btnNewButton_1);
 
 		JButton btnNewButton_2 = new JButton("Delete");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				scholarships.remove(list.getSelectedIndex());
+				data.saveData();
 			}
 		});
-		btnNewButton_2.setBounds(105, 424, 93, 29);
+		btnNewButton_2.setBounds(105, 394, 93, 29);
 		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Award Selection");
+		//Awards the scholarship that is selected
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				function.awardScholarship(scholarships.get(list.getSelectedIndex()));
+			}
+		});
+		btnNewButton_3.setBounds(20, 424, 135, 29);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Award All");
+		//Awards all available scholarships
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				function.awardAllScholarships();
+			}
+		});
+		btnNewButton_4.setBounds(155, 424, 135, 29);
+		contentPane.add(btnNewButton_4);
 	}
 }
